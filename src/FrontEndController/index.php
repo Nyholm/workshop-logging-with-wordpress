4afc7bd7bc94fd49070e6d56b1aec08e9d81962f
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-11-22 16:24:23 +0100

    More expressive logging

--- src/FrontEndController/index.php
+++ src/FrontEndController/index.php
@@ -2,8 +2,6 @@
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
  */
 
 /**
@@ -15,15 +13,15 @@ define('WP_USE_THEMES', true);
 
 try {
     /** Loads the WordPress Environment and Template */
-    require(dirname(__FILE__).'/wp-blog-header.php');
-} catch (Exception $e){
+    require dirname(__FILE__).'/wp-blog-header.php';
+} catch (Exception $e) {
     require_once __DIR__.'/wp-content/plugins/logging-with-monolog/logging-with-monolog.php';
-    addLogEntry('alert', $e->getMessage());
+    addLogEntry('alert', 'Uncaught exception', ['exception' => $e]);
 
     throw $e;
-} catch (Throwable $x){
+} catch (Throwable $x) {
     require_once __DIR__.'/wp-content/plugins/logging-with-monolog/logging-with-monolog.php';
-    addLogEntry('alert', $x->getMessage());
+    addLogEntry('alert', 'Uncaught exception', ['exception' => $x]);
 
     throw $x;
 }
