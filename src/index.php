4f3613895b2e0ec03548f919b94aa77cb10d4f6f
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-11-22 19:50:07 +0100

    Added use statement

--- src/index.php
+++ src/index.php
@@ -7,13 +7,16 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
+use Monolog\Handler;
+use Monolog\Processor;
+use Monolog\Formatter;
 
 // create a log channel
 $log = new Logger('my_logger');
 
-$handler = new StreamHandler(__DIR__.'/../logs/app.log', Logger::WARNING);
+$handler = new Handler\StreamHandler(__DIR__.'/../logs/app.log', Logger::WARNING);
 $log->pushHandler($handler);
+
 
 // add records to the log
 $log->warning('Foo');
